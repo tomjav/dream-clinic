be53c0a896b9726c1957942cadf1548c4a0584a9
tomjav <tomasz.grzybowski94@gmail.com>
2017-09-08 00:20:05 +0200

    Project Init

---
+++ src/test/java/pl/tgrzybowski/dreamclinic/DreamClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tgrzybowski.dreamclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DreamClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
